e8d4a37e57584fffc4fc08ec7e34a0074d8b64e3
saumG <saumster123@gmail.com>
2023-12-28 01:17:26 -0800

    created basic start and end node with nice grid an button for algorithm

---
+++ path-visualizer/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import logo from "./logo.svg";
+import Board from "./PathVisualizer/Board";
+
+function App() {
+  return (
+    <div className="App">
+      <Board></Board>
+    </div>
+  );
+}
+
+export default App;
---
+++ path-visualizer/src/PathVisualizer/Board.jsx
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import "./Board.css";
+import Node from "./Node/Node";
+
+const MAX_ROW_NUM = 20;
+const MAX_COL_NUM = 50;
+
+const START_NODE_ROW = 10;
+const START_NODE_COL = 15;
+const FINISH_NODE_ROW = 10;
+const FINISH_NODE_COL = 40;
+
+export default class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      grid: [],
+      mouseIsPressed: false,
+    };
+  }
+
+  componentDidMount() {
+    const grid = createInitialGrid();
+    this.setState({ grid });
+  }
+
+  handleMouseDown(row, col) {
+    console.log(`row and col are ${row}, ${col}`);
+    const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+    this.setState({ grid: newGrid, mouseIsPressed: true });
+  }
+
+  handleMouseEnter(row, col) {
+    if (this.state.mouseIsPressed) {
+      const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+      this.setState({ grid: newGrid });
+    }
+  }
+
+  handleMouseUp() {
+    this.setState({ mouseIsPressed: false });
+  }
+
+  render() {
+    const { grid, mouseIsPressed } = this.state;
+
+    return (
+      <>
+        <button>Visualize Dijkstra's Algorithm</button>
+        <div className="grid">
+          {grid.map((row, rowIdx) => {
+            return (
+              <div key={rowIdx}>
+                {row.map((node, nodeIdx) => {
+                  const { row, col, isFinish, isStart, isWall } = node;
+                  return (
+                    <Node
+                      key={nodeIdx}
+                      col={col}
+                      isFinish={isFinish}
+                      isStart={isStart}
+                      isWall={isWall}
+                      mouseIsPressed={mouseIsPressed}
+                      onMouseDown={(row, col) => this.handleMouseDown(row, col)}
+                      onMouseEnter={(row, col) =>
+                        this.handleMouseEnter(row, col)
+                      }
+                      onMouseUp={() => this.handleMouseUp()}
+                      row={row}
+                    ></Node>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
+}
+
+const createInitialGrid = () => {
+  const grid = [];
+  for (let row = 0; row < MAX_ROW_NUM; row++) {
+    const currentRow = [];
+    for (let col = 0; col < MAX_COL_NUM; col++) {
+      currentRow.push(createNode(row, col));
+    }
+    grid.push(currentRow);
+  }
+  return grid;
+};
+
+const createNode = (row, col) => {
+  return {
+    row,
+    col,
+    isStart: row === START_NODE_ROW && col === START_NODE_COL,
+    isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+    distance: Infinity,
+    isVisited: false,
+    isWall: false,
+    previousNode: null,
+  };
+};
+
+const getNewGridWithWallToggled = (grid, row, col) => {
+  const newGrid = grid.slice();
+  const node = newGrid[row][col];
+  const newNode = {
+    ...node,
+    isWall: !node.isWall,
+  };
+  newGrid[row][col] = newNode;
+  return newGrid;
+};
